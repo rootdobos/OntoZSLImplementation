1a4302c8497ccb5ee1d7586b736dd4ace14cf1ac
rootdobos <49080519+rootdobos@users.noreply.github.com>
2022-12-08 23:32:30 +0200

    changed generator learning with classifier

--- gan.py
+++ gan.py
@@ -27,30 +27,39 @@ def discriminator_loss(real_output,fake_output):
 def generator_loss(fake_output):
     cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     return cross_entropy(tf.ones_like(fake_output),fake_output)
-
+def generator_loss_classifier(prediction, ground_truth):
+    sc_cross_entropy= tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+    return sc_cross_entropy(ground_truth,prediction)
 @tf.function
-def train_step(features, seeds_from_ontology, generator,discriminator,generator_optimizer,discriminator_optimizer):
+#def train_step(features, seeds_from_ontology, generator,discriminator,generator_optimizer,discriminator_optimizer):
+def train_step( seeds_from_ontology, generator,classifier,ground_truth,generator_optimizer):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         generated_features=generator(seeds_from_ontology,training=True)
 
-        real_output= discriminator(features, training=True)
-        fake_output= discriminator(generated_features,training=True)
-
-        gen_loss= generator_loss(fake_output)
-        disc_loss= discriminator_loss(real_output,fake_output)
+        #real_output= discriminator(features, training=True)
+        #fake_output= discriminator(generated_features,training=True)
+        prediction =classifier(generated_features,training=False)
+        # print(prediction.shape)
+        # print(ground_truth.shape)
+        #gen_loss= generator_loss(fake_output)
+        #disc_loss= discriminator_loss(real_output,fake_output)
+        gen_loss= generator_loss_classifier(prediction,ground_truth)
     gradients_of_generator= gen_tape.gradient(gen_loss,generator.trainable_variables)
-    gradients_of_discriminator= disc_tape.gradient(disc_loss,discriminator.trainable_variables)
+    #gradients_of_discriminator= disc_tape.gradient(disc_loss,discriminator.trainable_variables)
 
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-    return gen_loss,disc_loss
+    #discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    #return gen_loss,disc_loss
+    return gen_loss
 
-def train(features, seeds,epochs, generator, discriminator, generator_optimizer,discriminator_optimizer,checkpoint,checkpoint_prefix):
+#def train(features, seeds,epochs, generator, discriminator, generator_optimizer,discriminator_optimizer,checkpoint,checkpoint_prefix):
+def train(seeds,epochs, generator, classifier,ground_truth, generator_optimizer,checkpoint,checkpoint_prefix):
     for epoch in range(epochs):
         start=time.time()
-        for i in range(features.shape[0]):
-            gen_loss, disc_loss=train_step(features[i],seeds[i],generator,discriminator,generator_optimizer,discriminator_optimizer)
+        for i in range(seeds.shape[0]):
+            #gen_loss, disc_loss=train_step(features[i],seeds[i],generator,discriminator,generator_optimizer,discriminator_optimizer)
+            gen_loss=train_step(seeds[i],generator,classifier,ground_truth[i],generator_optimizer)
         if(epoch+1)%15==0:
             checkpoint.save(file_prefix=checkpoint_prefix)
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-        print('Generator loss: {} Discriminator loss {} sec'.format(gen_loss,  disc_loss))
+        print('Generator loss: {} '.format(gen_loss))
