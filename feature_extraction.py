3b8ad7ca4bcdaef69b2de1d36618cf23c00dbcd2
rootdobos <49080519+rootdobos@users.noreply.github.com>
2022-12-09 11:59:01 +0200

    final

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+import os
 #https://pyimagesearch.com/2020/04/27/fine-tuning-resnet-with-keras-tensorflow-and-deep-learning/
 #https://www.tensorflow.org/tutorials/images/transfer_learning
 
@@ -57,3 +60,58 @@ def classifier_model(output_classes):
     x= tf.keras.layers.Dense(output_classes,activation='softmax')(x)
     model= tf.keras.Model(i,x)
     return model
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, cmap=plt.cm.Blues, png_output=None, show=True):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        title='Normalized confusion matrix'
+    else:
+        title='Confusion matrix'
+
+    # Calculate chart area size
+    leftmargin = 0.5 # inches
+    rightmargin = 0.5 # inches
+    categorysize = 0.5 # inches
+    figwidth = leftmargin + rightmargin + (len(classes) * categorysize)           
+
+    f = plt.figure(figsize=(figwidth, figwidth))
+
+    # Create an axes instance and ajust the subplot size
+    ax = f.add_subplot(111)
+    ax.set_aspect(1)
+    f.subplots_adjust(left=leftmargin/figwidth, right=1-rightmargin/figwidth, top=0.94, bottom=0.1)
+
+    res = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+
+    plt.title(title)
+    plt.colorbar(res)
+    ax.set_xticks(range(len(classes)))
+    ax.set_yticks(range(len(classes)))
+    ax.set_xticklabels(classes, rotation=45, ha='right')
+    ax.set_yticklabels(classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, format(cm[i, j], fmt),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black")
+
+    # plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    if png_output is not None:
+        os.makedirs(png_output, exist_ok=True)
+        f.savefig(os.path.join(png_output,'confusion_matrix.png'), bbox_inches='tight')
+
+    if show:
+        plt.show()
+        plt.close(f)
+    else:
+        plt.close(f)
